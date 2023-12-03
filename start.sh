f164dcc44428e42e973476e55dbb88a70b71e50a
asdfhjhk <152753619+asdfhjhk@users.noreply.github.com>
2023-12-03 02:47:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=6be7d2e2-7c47-43fc-8fb8-b98618f83892
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=skilletsandpots.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
